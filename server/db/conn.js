bf91dfd2fcca629b161105af3b2649678cd9c3a4
varsha <varshasawner9@gmail.com>
2023-02-10 14:57:07 +0530

    changes

--- server/db/conn.js
+++ server/db/conn.js
@@ -4,10 +4,9 @@ const mongoose = require('mongoose');
 // dotenv.config({path: "./config.env"})
 require('dotenv').config({ path: "./config.env" });
 
-// const DB = "mongodb+srv://varsha:tdf3iL6XDFr8hfY0@cluster0.w9f7sj1.mongodb.net/?retryWrites=true&w=majority";
 const DB = process.env.DATABASE;
 mongoose.set("strictQuery", false);
-mongoose.connect("mongodb+srv://varsha:tdf3iL6XDFr8hfY0@cluster0.w9f7sj1.mongodb.net/?retryWrites=true&w=majority", {
+mongoose.connect(DB, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => {
